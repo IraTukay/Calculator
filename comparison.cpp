b6203b0c2e09f3db4ee676ef3ccbfec02f4f3c70
IraTukay <121684650+IraTukay@users.noreply.github.com>
2022-12-30 22:45:31 +0300

    Update comparison.cpp

--- comparison.cpp
+++ comparison.cpp
@@ -5,8 +5,20 @@ bool descending(int x, int y) {
     return x<y;
 }
 bool isEqual(int x, int y){
-    return x=y;
+    return x==y;
 }
 bool isNotEqual(int x, int y) {
+    return !isEqual(x, y);
+}
+bool ascending(double x, double y) {
+    return x>y;
+}
+bool descending(double x, double y) {
+    return x<y;
+}
+bool isEqual(double x, double y){
+    return x==y;
+}
+bool isNotEqual(double x, double y) {
     return !isEqual(x, y);
 }
