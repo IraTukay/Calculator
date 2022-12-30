8eec0c0dea8aedf7663daadb614304f5c065dec5
IraTukay <121684650+IraTukay@users.noreply.github.com>
2022-12-30 21:23:33 +0300

    Update comparison.h

--- comparison.h
+++ comparison.h
@@ -8,7 +8,10 @@ bool ascending(int x, int y);
 bool descending(int x, int y);
 bool isEqual(int x, int y);
 bool isNotEqual(int x, int y);
-
+bool ascending(double x, double y);
+bool descending(double x, double y);
+bool isEqual(double x, double y);
+bool isNotEqual(double x, double y);
 
 
 #endif //CALCULATOR_COMPARISON_H
