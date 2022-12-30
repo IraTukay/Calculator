221d92bd375bd4930faaef4b6cf73c3872c39bce
IraTukay <121684650+IraTukay@users.noreply.github.com>
2022-12-30 21:00:20 +0300

    Add files via upload

---
+++ InputOutputFunctions.h
@@ -0,0 +1,10 @@
+
+#ifndef CALCULATOR_INPUTOUTPUTFUNCTIONS_H
+#define CALCULATOR_INPUTOUTPUTFUNCTIONS_H
+
+#include "InputOutputFunctions.cpp"
+int BinaryResult(int a, int b, operations sign);
+int UnaryResult(int a, operations sign);
+double UnaryResult(double a, operations sign);
+
+#endif //CALCULATOR_INPUTOUTPUTFUNCTIONS_H
